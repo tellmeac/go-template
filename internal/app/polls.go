f67747013634692f9759e3cccf836c34e9f1a869
Alexander Lipatov <tellmeac@gmail.com>
2022-12-24 11:54:19 +0700

    Implement app poll methods

--- internal/app/polls.go
+++ internal/app/polls.go
@@ -3,14 +3,23 @@ package app
 import (
 	"context"
 	"github.com/tellmeac/go-template/internal/model"
+	"net/http"
 )
 
 func (a App) AllPolls(ctx context.Context) ([]model.Poll, error) {
-	//TODO implement me
-	panic("implement me")
+	polls, err := a.store.Polls().All(ctx)
+	if err != nil {
+		return nil, model.NewError("failed to get all polls").Wrap(err).WithCode(http.StatusInternalServerError)
+	}
+	return polls, nil
 }
 
-func (a App) CreatePoll(ctx context.Context, poll model.PollPost) error {
-	//TODO implement me
-	panic("implement me")
+func (a App) CreatePoll(ctx context.Context, request model.PollPost) error {
+	poll := model.NewPoll(request.Question).WithAnswers(request.AnswersContent...)
+
+	err := a.store.Polls().Create(ctx, *poll)
+	if err != nil {
+		return model.NewError("failed to save new poll").Wrap(err).WithCode(http.StatusInternalServerError)
+	}
+	return nil
 }
