f18e187a0e6e0d69b5d274f67aa48b61fc294f5e
Alexander Lipatov <tellmeac@gmail.com>
2023-02-16 21:05:43 +0700

    Remove ctx logging for now

--- internal/core/actions/user.go
+++ internal/core/actions/user.go
@@ -3,7 +3,6 @@ package actions
 import (
 	"context"
 	"github.com/tellmeac/go-template/internal/storage/db"
-	"github.com/tellmeac/go-template/pkg/ctxlog"
 	"go.uber.org/zap"
 
 	"github.com/google/uuid"
@@ -11,8 +10,7 @@ import (
 )
 
 func GetUser(ctx context.Context, uid uuid.UUID, db *db.Storage) (*entities.User, error) {
-	ctx = ctxlog.WithFields(ctx, zap.Any("uid", uid))
-	ctxlog.Ctx(ctx, zap.L()).Info("Get user action")
+	zap.L().Info("GetUser", zap.Any("uid", uid))
 
 	return db.User.Get(ctx, uid)
 }
